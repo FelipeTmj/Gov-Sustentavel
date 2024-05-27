1f32f49248fb99041e7169089093ccb7f4579352
Felipe <104326732+FelipeTmj@users.noreply.github.com>
2024-05-27 18:18:29 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal ({ reset: true });
+
+sr.reveal('.container-area1', { duration: 1500 });
+sr.reveal('.sustainabilty', { duration: 1500 });
+sr.reveal('.photo', { duration: 1400 });
+sr.reveal('.container-area3', { duration: 1500 });
+sr.reveal('.container-area4', { duration: 1500 });
+sr.reveal('container-area5', { duration: 1500 });
+
+function redirect() {{
+      window.location.href = 'https://unipead-my.sharepoint.com/:w:/g/personal/felipe_oliveira290_aluno_unip_br/Ea4m_Mn2FUhJpq3_vvjnuDYB30suvptSuNe9Ni3qdAvbGA?e=lRrlMJ';
+    }
+  }
